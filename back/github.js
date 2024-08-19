189795b015ad51f58503fca78757f88f15a3d459
AlmaCRH <alma5crh@gmail.com>
2024-08-19 12:22:13 +0100

    Adapted code to use Promise.all for optimize purpose

--- back/github.js
+++ back/github.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
 
+const appID = process.env.APP_ID;
+const privateKey = process.env.PRIVATE_KEY.replace(/\\n/g, "\n");
+const installationID = process.env.INSTALLATION_ID;
+const organization = process.env.ORG;
+
 const loadApp = async () => {
   try {
     const { App } = await import("@octokit/app");
@@ -10,17 +15,12 @@ const loadApp = async () => {
   }
 };
 
-const appID = process.env.APP_ID;
-const privateKey = process.env.PRIVATE_KEY.replace(/\\n/g, "\n");
-const installationID = process.env.INSTALLATION_ID;
-
-
 // Pull request filter
 const listAllPulls = async () => {
   try {
     const octokit = await loadApp();
     const { data } = await octokit.request("GET /repos/{owner}/{repo}/pulls", {
-      owner: process.env.OWNER,
+      owner: organization,
       repo: "LAB-110-html-tables-forms",
       headers: {
         "X-GitHub-Api-Version": "2022-11-28",
@@ -36,37 +36,50 @@ const listAllPulls = async () => {
 };
 
 const filterPulls = async () => {
-  const allPulls = await listAllPulls();
-  const usersList = await arrayMembers();
-
-  const pulls = allPulls
-    .filter((el) => usersList.includes(el.user.login))
-    .map((el) => ({ created_at: el.created_at, user: el.user.login }));
-  return pulls;
+  try {
+    const [allPulls, usersList] = await Promise.all([
+      await listAllPulls(),
+      await arrayMembers()
+    ])
+    const pulls = allPulls
+      .filter((el) => usersList.includes(el.user.login))
+      .map((el) => ({ created_at: el.created_at, user: el.user.login }));
+    return pulls;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // Teams filter
 
 const getAllTeamMembers = async () => {
-  const octokit = await loadApp();
-  const { data } = await octokit.request(
-    "GET /orgs/{org}/teams/{team_slug}/members",
-    {
-      org: "rebootacademy-labs",
-      team_slug: "SCE-6",
-      per_page: 50,
-      headers: {
-        "X-GitHub-Api-Version": "2022-11-28",
-      },
-    }
-  );
-  return data;
+  try {
+    const octokit = await loadApp();
+    const { data } = await octokit.request(
+      "GET /orgs/{org}/teams/{team_slug}/members",
+      {
+        org: organization,
+        team_slug: "SCE-6",
+        per_page: 50,
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const arrayMembers = async () => {
-  const teamMembers = await getAllTeamMembers();
-  const members = teamMembers.map((user) => user.login);
-  return members;
+  try {
+    const teamMembers = await getAllTeamMembers();
+    const members = teamMembers.map((user) => user.login);
+    return members;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = filterPulls;
--- back/index.js
+++ back/index.js
@@ -66,114 +66,127 @@ async function authorize() {
 }
 
 /**
- * Prints the names and majors of students in a sample spreadsheet:
- * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
-async function writeIntersection(auth) {
+const writeIntersection = async (auth) => {
   try {
     const sheets = google.sheets({ version: "v4", auth });
-    const githubData = await filterPulls();
+    /*const githubData = await filterPulls();
     const indexBlock = await getIndexBlock(sheets, "BLOCK 1");
     const columnIndex = await getColumnIndexFromLabName(
       sheets,
       indexBlock,
       "JS Animations"
-    );
-
-    return githubData.map(async (pull) => {
-      const rowIndex = await getRowIndexFromGithubUser(
-        sheets,
-        indexBlock,
-        pull.user
-      );
-
-      if (columnIndex && rowIndex) {
-        const res = sheets.spreadsheets.values.update({
-          spreadsheetId: process.env.SPREADSHEET_ID,
-          range: `Lab Tracker!${columnIndex}${rowIndex}`,
-          valueInputOption: "USER_ENTERED",
-          requestBody: {
-            values: [[""]],
-          },
-        });
-        return res;
-      }
-    });
+    ); */
+    const [githubData, columnIndex] = await Promise.all([
+      await filterPulls(),
+      await getColumnIndexFromLabName(sheets, "BLOCK 1", "JS Animations"),
+    ]);
+    if (githubData && githubData.length > 0) {
+      const promiseList = githubData.map(async (pull) => {
+        try {
+          const rowIndex = await getRowIndexFromGithubUser(
+            sheets,
+            "BLOCK 1",
+            pull.user
+          );
+          if (columnIndex && rowIndex) {
+            return sheets.spreadsheets.values.update({
+              spreadsheetId: process.env.SPREADSHEET_ID,
+              range: `Lab Tracker!${columnIndex}${rowIndex}`,
+              valueInputOption: "USER_ENTERED",
+              requestBody: {
+                values: [["Fix"]],
+              },
+            });
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      });
+      await Promise.all(promiseList);
+    }
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 const getIndexBlock = async (sheets, block) => {
-  const res = await sheets.spreadsheets.values.get({
-    spreadsheetId: process.env.SPREADSHEET_ID,
-    range: "Lab Tracker!A:A",
-  });
+  try {
+    const res = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.SPREADSHEET_ID,
+      range: "Lab Tracker!A:A",
+    });
 
-  const columns = res.data.values;
-  if (!columns || columns.length === 0) {
-    console.log("No data found.");
-    return;
-  }
-  if (columns.length !== 0) {
-    for (let i = 0; i < columns.length; i++) {
-      if (columns[i][0] === block.toUpperCase()) {
-        return i + 1;
+    const columns = res.data.values;
+    if (!columns || columns.length === 0) {
+      console.log("No data found.");
+      return;
+    }
+    if (columns.length !== 0) {
+      for (let i = 0; i < columns.length; i++) {
+        if (columns[i][0] === block.toUpperCase()) {
+          return i + 1;
+        }
       }
     }
+  } catch (error) {
+    console.log(error);
   }
 };
 
 const getRowIndexFromGithubUser = async (sheets, block, githubName) => {
-  const res = await sheets.spreadsheets.values.get({
-    spreadsheetId: process.env.SPREADSHEET_ID,
-    range: "Lab Tracker!B:B",
-  });
-  const rows = res.data.values;
+  try {
+    const res = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.SPREADSHEET_ID,
+      range: "Lab Tracker!B:B",
+    });
+    const rows = res.data.values;
 
-  if (!rows || rows.length === 0) {
-    console.log("No data found.");
-    return;
-  }
-  if (rows.length !== 0) {
-    for (let i = block; i < rows.length; i++) {
+    if (!rows || rows.length === 0) {
+      console.log("No data found.");
+      return;
+    }
+    for (let i = 2; i < rows.length; i++) {
       if (rows[i][0] === githubName) {
         return i + 1;
       }
     }
+  } catch (error) {
+    console.log(error);
   }
 };
 
 const getColumnIndexFromLabName = async (sheets, block, labName) => {
-  const res = await sheets.spreadsheets.values.get({
-    spreadsheetId: process.env.SPREADSHEET_ID,
-    range: "Lab Tracker!D:R",
-  });
-  const columns = res.data.values;
-  if (!columns || columns.length === 0) {
-    console.log("No data found.");
-    return;
-  }
-  if (columns.length !== 0) {
-    for (let i = block; i < columns.length; i++) {
-      if (columns[i + 1].length === 0) return;
+  try {
+    const res = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.SPREADSHEET_ID,
+      range: "Lab Tracker!D:R",
+    });
+    const columns = res.data.values;
+    if (!columns || columns.length === 0) {
+      console.log("No data found.");
+      return;
+    }
+    for (let i = 2; i < columns.length; i++) {
       for (let j = 0; j < columns[i].length; j++) {
-        if (columns[i][j] === labName) {
+        if (columns[i][j] === labName && columns[i][j] !== undefined) {
           return convertIndexToLetter(j + 3);
         }
       }
     }
+  } catch (error) {
+    console.log(error);
   }
 };
 
-function convertIndexToLetter(index) {
+const convertIndexToLetter = (index) => {
   let letter = "";
   while (index >= 0) {
     letter = String.fromCharCode((index % 26) + 65) + letter;
     index = Math.floor(index / 26) - 1;
   }
   return letter;
-}
+};
 
 authorize().then(writeIntersection).catch(console.error);
