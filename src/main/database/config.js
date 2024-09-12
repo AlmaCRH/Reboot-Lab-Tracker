dc4b8c66a83b409902086b9db0d67bbb60ced2c0
AlmaCRH <alma5crh@gmail.com>
2024-09-12 16:58:51 +0100

    GENERAL DATABASE/API - Created a basic structure (CRUD) for a REST API

--- src/main/database/config.js
+++ src/main/database/config.js
@@ -13,7 +13,11 @@ const checkAndSync = async () => {
 
 const initializeAndListen = async () => {
   try {
-    const app = express().use(cors()).use(morgan("dev")).use(express.json());
+    const app = express()
+      .use(cors())
+      .use(morgan("dev"))
+      .use(express.json())
+      .use("/api", require("./api/router"));
     app.listen(port, () => console.log(`Server started on port ${port}`));
   } catch (error) {
     console.error(error);
